2ad9eac52912d6a75431aefb6149909fd555d72d
CarlosG18 <c.gabriel.abc18@gmail.com>
2023-05-15 08:01:28 -0300

    adicionando arquivos bases para o frontend

--- ecomerce/ecomerce/urls.py
+++ ecomerce/ecomerce/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='cliente/')),
     path('carrinho/', include('carrinho.urls')),
     path('produto/', include('produto.urls')),
     path('cliente/', include('cliente.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
