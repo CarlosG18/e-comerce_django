919f3f6f9a5c72bb3894eabdab5a1d30cdd816eb
CarlosG18 <c.gabriel.abc18@gmail.com>
2023-06-02 16:21:59 -0300

    tentativa

--- ecomerce/cliente/models.py
+++ ecomerce/cliente/models.py
@@ -5,23 +5,18 @@ from django.core.validators import RegexValidator
 from django.contrib.auth.models import User
 
 class Cliente(models.Model):
-  nome = models.CharField(max_length=200)
-  avatar = models.ImageField(upload_to="cliente/", default="cliente/avatar_default.png")
-  email = models.EmailField(help_text="insira seu email")
-
-  cpf_setting = RegexValidator(
-    regex=r'^\d{3}\.\d{3}\.\d{3}-\d{2}$',
-    message="o cpf informado deverá esta no formato 999.999.999-99",
-  )
-  
-  cpf = models.CharField(primary_key=True, max_length=14, validators=[cpf_setting], unique=True)
-
-  TYPE = (
-      ("f", "pessoa-fisica"),
-      ("e", "empresa"),
-    )
-    
-  type_cliente = models.CharField(max_length=1, choices=TYPE, help_text="tipo de cliente", default='f')
+  user = models.OneToOneField(User, on_delete=models.CASCADE, default=None)
+  #avatar = models.ImageField(upload_to="cliente/", default="cliente/avatar_default.png")
+  #cpf_setting = RegexValidator(
+    #regex=r'^\d{3}\.\d{3}\.\d{3}-\d{2}$',
+   # message="o cpf informado deverá esta no formato 999.999.999-99",
+ # )
+  #cpf = models.CharField(primary_key=True, max_length=14, validators=[cpf_setting], unique=True)
+  #TYPE = (
+      #("f", "pessoa-fisica"),
+      #("e", "empresa"),
+    #)
+  #type_cliente = models.CharField(max_length=1, choices=TYPE, help_text="tipo de cliente", default='f')
   
   def __str__(self):
-    return f'cliente {self.nome}'
+    return f'cliente {self.user.username}'
--- ecomerce/cliente/views.py
+++ ecomerce/cliente/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
 from .models import Cliente
 from produto.models import Produto, ListImages, Categoria
-from .forms import FormCriarCliente
+from .forms import UserClienteForm
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 
-# @login_required
 def index(request):
   produtos = Produto.objects.filter(status="o")[:6]
   categorias = Categoria.objects.all()
@@ -18,14 +17,12 @@ def index(request):
 
 def criar_conta(request):
   if request.method == "POST":
-    form = FormCriarCliente(request.POST)
+    form = UserClienteForm(request.POST)
     if form.is_valid():
-      user = User.objects.create_user(username=form.cleaned_data['nome'],email=form.cleaned_data['email'],password=form.cleaned_data['cpf'])
-      user.save()
       form.save()
       return HttpResponseRedirect(reverse('cliente:index'))
   else:
-    form = FormCriarCliente()
+    form = UserClienteForm()
   return render(request, "cliente/criar_conta.html", {
       "form": form,
   })
